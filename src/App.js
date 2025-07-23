5fdb5601e57a6d76c74e63320e7bd8e16179d1bc
Amir Mukhametov <ggaben61@gmail.com>
2025-07-23 17:08:01 +0300

    Какая то ебучая ошибка

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Block } from './Block';
 import './index.scss';
 
 function App() {
+  const [fromCurrency, setFromCurrency] = useState('RUB');
+  const [toCurrency, setToCurrency] = useState('AED'); 
+  const [fromPrice, setFromPrice] = useState(0);
+  const [toPrice, setToPrice] = useState(0);
+
+  const[rates, setRates] = useState({});
+
+  useEffect(() => {
+    fetch('https://www.cbr-xml-daily.ru/latest.js')
+      .then(res => res.json())
+      .then(json => {
+        console.log('Загруженные курсы:', json.rates); 
+        setRates(json.rates)
+      })
+      .catch(err => {
+        console.warn(err);
+        alert('Не уадлось получить информацию')
+      })
+  },[])
+
+  const onCahngeToPrice = (value) => {
+    value = Number(value);
+    console.log('Введено в toPrice:', value); 
+    const price = value / rates[fromCurrency];
+    const result = price * rates[toCurrency];
+    setToPrice(value);
+    setFromPrice(result);
+  }
+
+  const onCahngeFromPrice = (value) => {
+    setFromPrice(value)
+  }
+
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block 
+        value={fromPrice} 
+        currency={fromCurrency} 
+        onChangeCurrency={setFromCurrency} onChangeValue={onCahngeFromPrice} 
+      />
+      <Block 
+        value={toPrice} 
+        currency={toCurrency} 
+        onChangeCurrency={setToCurrency} onChangeValue={onCahngeToPrice} 
+      />
     </div>
   );
 }
